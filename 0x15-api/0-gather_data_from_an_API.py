08acf4f1e9aa96071bd3c0153c4e851e32a40153
bsmtt <basmatalaat224@gmail.com>
2024-06-03 15:26:05 -0700

    feat: gather_data_from_an_API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,9 +9,10 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(user_id)).json()
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    completed = [todo.get("title") for todo in todos if todo.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
-                                                          len(completed), 
-                                                          len(todos)))
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"),
+        len(completed),
+        len(todos))
+    )
     [print("\t {}".format(c)) for c in completed]
-
