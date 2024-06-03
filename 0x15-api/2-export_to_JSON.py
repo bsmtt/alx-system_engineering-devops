b8b539fa82ec4e118e014843cdcab677ad3282c7
bsmtt <basmatalaat224@gmail.com>
2024-06-03 16:29:29 -0700

    feat: 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(user_id)).json()
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, user.get("username"), t.get("completed"), t.get("title")]
-            ) for t in todos]
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": user.get("username")
+            } for t in todos]}, jsonfile)
